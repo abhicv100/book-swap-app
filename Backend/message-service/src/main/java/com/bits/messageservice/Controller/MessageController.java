77a6277ffb765f1710c0ed1664d94a65673cc261
abhicv100 <2023mt93301@wilp.bits-pilani.ac.in>
2024-04-27 12:16:47 +0530

    added logger in MessageController, changed response json structure

--- Backend/message-service/src/main/java/com/bits/messageservice/Controller/MessageController.java
+++ Backend/message-service/src/main/java/com/bits/messageservice/Controller/MessageController.java
@@ -2,29 +2,31 @@ package com.bits.messageservice.Controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bits.messageservice.Dao.MessageDao;
 import com.bits.messageservice.Entity.MessageEntity;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
-@ResponseBody
 public class MessageController {
 
     @Autowired
     MessageDao messageDao;
+
+    Logger logger = LoggerFactory.getLogger(MessageController.class);
 
     String getMessageChannelId(String fromUid, String toUid) {
         String channelId;
@@ -39,45 +41,77 @@ public class MessageController {
     }
 
     @PostMapping("/messages")
-    public Map<String, Object> sendMessage(@RequestBody MessageEntity message) {
+    public ResponseEntity<Map<String, Object>> sendMessage(@RequestBody MessageEntity message) {
+
+        logger.info("Message send from '{}' to '{}'", message.getSenderId(), message.getReceiverId());
+
         Map<String, Object> result = new HashMap<>();
-        
-        String messageId = UUID.randomUUID().toString().replace("-", "");
-        message.setId(messageId);
+        try {
+            String messageId = UUID.randomUUID().toString().replace("-", "");
+            message.setId(messageId);
+    
+            String channelId = getMessageChannelId(message.getSenderId(), message.getReceiverId());
+            message.setChannelId(channelId);
+    
+            messageDao.save(message);
+    
+            result.put("data", messageId);
+            result.put("status", "success");
 
-        String channelId = getMessageChannelId(message.getSenderId(), message.getReceiverId());
-        message.setChannelId(channelId);
-
-        messageDao.save(message);
-
-        result.put("data", messageId);
-        result.put("status", "success");
-        return result;
+            return ResponseEntity.ok().body(result);
+        } catch(Exception e) {
+            logger.error("error: {}", e.getMessage());
+            result.put("error_msg", "failed to sent message");
+            result.put("status", "fail");
+            return ResponseEntity.internalServerError().body(result);
+        }
     }
 
-    // NOTE: for polling for new messages from frontend
-    // TODO: accept list of channel id and serach after timestamp
+    // NOTE: for polling new messages from UI
+    // TODO: accept list of channel id and search after timestamp
     @GetMapping("/messages/channel/{channelId}")
-    public Map<String, Object> getMessages(@PathVariable String channelId, @RequestParam(name="after", defaultValue="0") String after) {
+    public ResponseEntity<Map<String, Object>> getMessages(@PathVariable String channelId, @RequestParam(name="after", defaultValue="0") long after) {
+        
+        logger.info("fetching message list for chat channel '{}'' after time '{}'", channelId, after);
         
         Map<String, Object> result = new HashMap<>();
 
-        long searchAfterTimeStamp = Long.parseLong(after);
-
-        var messages = messageDao.findByChannelId(channelId);
-
-        result.put("data", messages);
-        result.put("status", "success");
-        
-        return result;
+        try {
+            var messages = messageDao.findByChannelIdAndTimeStampGreaterThan(channelId, after);
+            result.put("data", messages);
+            result.put("status", "success");   
+            return ResponseEntity.ok().body(result);
+        } catch(Exception e) {
+            logger.error("error: {}", e.getMessage());
+            result.put("error_msg", "failed to get messages");
+            result.put("status", "fail");
+            return ResponseEntity.internalServerError().body(result);
+        }
     }
 
     @GetMapping("/messages/user/{userId}")
-    public Map<String, Object> getChannelIdsForUser(@PathVariable String userId) {
+    public ResponseEntity<Map<String, Object>> getChannelIdsForUser(@PathVariable String userId) {
+
+        logger.info("fetching channel ids for user '{}'", userId);
+
         Map<String, Object> result = new HashMap<>();
-        List<MessageEntity> messages = messageDao.findBySenderIdOrReceiverId(userId, userId);
-        result.put("data", messages);
-        result.put("status", "success");
-        return result;
+
+        try {
+            List<String> channelIds = messageDao.findDistinctChannelIdBySenderIdOrReceiverId(userId, userId);
+
+            // channelIds.forEach((id) -> {
+            //     String othersId = id.replace(userId, "");
+            //     System.out.println(othersId);
+            // });
+
+            result.put("data", channelIds);
+            result.put("status", "success");
+            return ResponseEntity.ok().body(result);
+        } catch(Exception e) {
+            logger.error("error: {}", e.getMessage());
+            result.put("error_msg", "failed to get messages");
+            result.put("status", "fail");
+            return ResponseEntity.internalServerError().body(result);
+        }
     }
 }
--- Backend/message-service/src/main/java/com/bits/messageservice/Dao/MessageDao.java
+++ Backend/message-service/src/main/java/com/bits/messageservice/Dao/MessageDao.java
@@ -1,5 +1,6 @@
 package com.bits.messageservice.Dao;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,9 @@ import java.util.List;
 
 @Repository
 public interface MessageDao extends ListCrudRepository<MessageEntity, String> {
-    List<MessageEntity> findByChannelId(String channelId);
-    List<MessageEntity> findBySenderIdOrReceiverId(String senderId, String receiverId);
+
+    List<MessageEntity> findByChannelIdAndTimeStampGreaterThan(String channelId, long after);
+
+    @Query(value = "SELECT DISTINCT m.channelId FROM MessageEntity m WHERE m.senderId=?1 OR m.receiverId=?2")
+    List<String> findDistinctChannelIdBySenderIdOrReceiverId(String senderId, String receiverId);
 }
