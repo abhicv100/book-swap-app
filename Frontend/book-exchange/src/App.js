fbd3b0c7f7961f8c02f0df513fc72eb2b49916ca
AchyuthBITS <2023mt93300@wilp.bits-pilani.ac.in>
2024-05-12 13:45:20 +0530

    this is 2nd commit with book list and book details page added

--- Frontend/book-exchange/src/App.js
+++ Frontend/book-exchange/src/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import { Login } from './Login';
 import { Register } from './Register';
 import { Home } from './Home';
 import Profile from './Profile'; // Import the Profile component
+import BookDetailsPage from './Bookdetails';
+import BookGrid from './Booklist';
 
 
 function App() {
@@ -15,17 +18,15 @@ function App() {
   }
 
   return (
-    <div className="App">
-      {currentForm === "login" ? (
-        <Login onFormSwitch={() => setCurrentForm('home')} />
-      ) : currentForm === "register" ? (
-        <Register onFormSwitch={toggleForm} />
-      ) : currentForm === "home" ? (
-        <Home onViewProfile={() => setCurrentForm('profile')} /> // Pass onViewProfile prop to Home component
-      ) : (
-        <Profile />
-      )}
+    <Router>
+    <div>
+      <Routes>
+        <Route path="/" element= {<Home/>} />
+        <Route path="/book" element={<BookDetailsPage/>} />
+      </Routes>
     </div>
+    </Router>
+
   );
 }
 
